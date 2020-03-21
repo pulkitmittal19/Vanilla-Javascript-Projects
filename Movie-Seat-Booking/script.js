3adabc82dd9653d19a8e93be2cd6f86bd969ef24
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-03-21 23:38:17 +0530

    Movie-Seat-Booking

---
+++ Movie-Seat-Booking/script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+
+let ticketPrice = +movieSelect.value;
+
+
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
+updateSelectedCount();
