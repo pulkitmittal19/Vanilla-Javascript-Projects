bcc9ee4436761ffa650024379ca27ddfa784fc21
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-03-23 22:00:42 +0530

    Added: Js file

--- Exchange-Rate/script.js
+++ Exchange-Rate/script.js
@@ -23,3 +23,18 @@ function caclulate() {
     });
 }
 
+// Event listeners
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  caclulate();
+});
+
+caclulate();
+
