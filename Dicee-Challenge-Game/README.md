27a94c49760bf5ad05d109190500ac061d9f5e1d
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-03-26 14:21:44 +0530

    Added:Dicee-Challenge

---
+++ Dicee-Challenge-Game/README.md
@@ -0,0 +1,10 @@
+## Dicee Challenge Game
+
+Dicee Challenge Game using Vanilla Javascript
+ 
+## Project Specifications 
+
+- Generates randon numbers on the dice on relods
+- Reload button
+- Display the result among two players
+
---
+++ Dicee-Challenge-Game/index.js
@@ -0,0 +1,42 @@
+
+
+//Generate random numbers
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage; 
+
+var image1 = document.querySelectorAll("img")[0];
+
+//using attributes to replace the image scr
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+//Player 2 wins
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+//Draw Condition
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+//Reload Property
+
+function reloadPage(){
+  location.reload(true);
+}
